ad3dad76297a5e3e189b52d2d14f70f8784a905b
Mayank Kandpal <mayankk4@gmail.com>
2016-04-08 20:38:45 +0530

    Barebones

---
+++ src/ConstraintSatisfaction.cpp
@@ -0,0 +1,34 @@
+//================================================================================
+// Name        : ConstraintSatisfaction.cpp
+// Author      : Mayank Kandpal
+// Version     : 0.1
+// Copyright   : 2016
+// Description : Implementing an algorithm for a Constraint Satisfaction Problem.
+//================================================================================
+
+/**
+ * Backtracking, CSP.
+ *
+ * Crossword	design
+ 	 There	is	a	board	of	size:	n x	m and	a	set	of	words.
+ 	 We	need	to	find	such	a	way	of	writing	words	on	the board	such	that	one	cell
+ 	 includes	at	most	one	letter	and	every	continuous	sequence	of	letters	read
+ 	 horizontally	and	vertically	is	a	word	from	a	subset	of	the	size	k of
+ 	 the	set	S acquired	in	the	following  way
+ 	 	 a) download	the	list	of	English	lemmas	extracted	from	the	British	National	Corpus	prepared	by
+ 	 	 	Prof.	Adam	Kilgarriff:	https://www.kilgarriff.co.uk/bnc-readme.html
+		 b) select	from	the	list	only:	adverbs	(adv),	verbs	(v),	adjectives	(a)	and	nouns	(n)
+		 	Represent	the	problem	as	a	CSP
+
+		2	points	– formulate	the	chosen	problem	as	CSP
+		3	points	– implement	backtracking
+		3	points	– compare	efficiency	of	processing	for	various	problem	parameters
+		2	points	– give	a	correct	interpretation	of	the	results
+ */
+
+#include <iostream>
+
+int main() {
+
+	return 0;
+}
